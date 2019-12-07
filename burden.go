de42258f1ec5c16d1eef6a036e2d4f021d073c60
christophersinclair <csinclair11@gmail.com>
2019-12-07 17:44:04 -0500

    Added a yaml config parser

--- burden.go
+++ burden.go
@@ -3,35 +3,13 @@ package main
 import (
 	"fmt"
 	//"time"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
 )
-
-type LoadConfig struct {
-	users int
-}
-
-var loadConfig LoadConfig
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func loadconf(filename string) []byte {
-	file, err := ioutil.ReadFile(filename) // TODO: change this to a cmd-line argument
-	check(err)
-	return file
-}
 
 func main() {
 	fmt.Println("Hello, World!")
 
-	config := loadconf("config\\simple-order.yml")
-	fmt.Println(string(config))
-
-	yaml.Unmarshal(config, &loadConfig)
+	config := loadconf("config\\simple-order.yml") // TODO: change to cmd-line argument (-f order.yml)
 	
-	fmt.Println(loadConfig)
+	fmt.Printf("--- m:\n%v\n\n", config)
+
 }
---
+++ loadconf.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+)
+
+type BurdenConfig struct {
+	Test struct {
+		users int
+	}
+}
+
+var burdenConfig BurdenConfig
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func loadconf(filename string) map[interface{}]interface{} {
+	conf, err := ioutil.ReadFile(filename)
+	check(err)
+	
+	m := make(map[interface{}]interface{})
+
+	err = yaml.Unmarshal([]byte(conf), &m)
+	check(err)
+
+	return m
+
+}
