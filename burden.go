3d0c6ac40671bffb376cf86757b3bd0725c8b0dd
christophersinclair <csinclair11@gmail.com>
2019-12-07 11:39:43 -0500

    Added config loader

--- burden.go
+++ burden.go
@@ -1,11 +1,37 @@
-package burden
+package main
 
 import (
 	"fmt"
-	"time"
-	"load"
-	"stress"
-
+	//"time"
 	"gopkg.in/yaml.v2"
+	"io/ioutil"
 )
 
+type LoadConfig struct {
+	users int
+}
+
+var loadConfig LoadConfig
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func loadconf(filename string) []byte {
+	file, err := ioutil.ReadFile(filename) // TODO: change this to a cmd-line argument
+	check(err)
+	return file
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	config := loadconf("config\\simple-order.yml")
+	fmt.Println(string(config))
+
+	yaml.Unmarshal(config, &loadConfig)
+	
+	fmt.Println(loadConfig)
+}
