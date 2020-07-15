ca21d3d1aaa9662a06e1a6d9e621008171936c69
christophersinclair <csinclair11@gmail.com>
2020-07-15 16:56:25 -0400

    Replaced yaml conf with dotenv

--- burden.go
+++ burden.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"fmt"
-	"flag"
+	"os"
+	"github.com/joho/godotenv"
+	"log"
 )
 
+
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+
+  // load .env file
+  err := godotenv.Load(".env")
+
+  if err != nil {
+    log.Fatalf("Error loading .env file")
+  }
+
+  return os.Getenv(key)
+}
+
 func main() {
-	confPtr := flag.String("f", "config/order.yml", "Location of order.yml")
-	flag.Parse()
+  // godotenv package
+  dotenv := goDotEnvVariable("stress_time")
 
-	config := loadconf(*confPtr)
-	
-	fmt.Printf("--- m:\n%v\n\n", config)
-
+  fmt.Printf("godotenv : %s = %s \n", "stress_time", dotenv)
 }
